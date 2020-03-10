020cbc31306a45d84613a4d5d28adb4c625f622a
Maria Danielson <mal3905@gmail.com>
2020-03-10 19:39:01 -0400

    minmaxsum in prog

---
+++ minmaxsum.js
@@ -0,0 +1,28 @@
+
+
+function miniMaxSum(arr) {
+    let min = arr[0]
+    let max = min
+    let sum = min
+
+    for (let i=0; i < arr.length; i++){
+        sum += arr[i]
+
+        if(arr[i] < min){
+            min = arr[i]
+        }
+        if(arr[i] > max ){
+            max = arr[i]
+        }
+    }
+
+   console.log( (sum - max) + ' ' + (sum - min))
+ }
+
+
+
+
+
+
+miniMaxSum([1, 4, 2, 5, 3])
+console.log(miniMaxSum)
